dd1b52fcb890401e7c8be5b04c7e262a22228a21
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2021-10-22 12:28:45 -0300

    Adição do projeto ao GitHub V0.8.0

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
+    products: [],
+    productsInBag: [],
   },
   mutations: {
+    loadProducts: function(state, products) {
+      state.products = products;
+    }
   },
   actions: {
+    loadProducts: function({ commit }) {
+      axios.get("https://fakestoreapi.com/products").then((response) => {
+        commit("loadProducts", response.data);
+      });
+    },
+    
   },
-  modules: {
-  }
-})
+  modules: {},
+});
